f0aa8ad788a5d378b516ef5dcb74d8a9dd29dee9
Don <lordashym@gmail.com>
2019-08-19 11:05:53 -0500

    done

---
+++ DogGenetics/DogGenetics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogGenetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 100;
+            int[] numbers = new int[5];
+            string[] breed = new string[] { "Golden Retriever", "Beagle", "Norwegian Elk Hound", "American Pit Bull", "Corgi" };
+            Console.WriteLine("What is your dogs name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Well then, I have this highly reliable report on " + name + " prestigous background right here.");
+
+            Console.WriteLine(name + " is:");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Random random = new Random();
+                numbers[i] = random.Next(1, total);
+                total = total - numbers[i];
+            }
+            numbers[4] = total;
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.WriteLine(numbers[j] + "% " + breed[j]);
+            }
+        }
+    }
+}
---
+++ HealthyHearts/HealthyHearts/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthyHearts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            int max = 220 - age;
+            double low = max * .5;
+            double high = max * .85;
+            low = Math.Round(low);
+            high = Math.Round(high);
+
+            Console.WriteLine("Your maximum heart rate should be " + max + " beats per minute");
+            Console.WriteLine("Your target HR zone is " + low + " - " + high + " beats per minute");
+        }
+    }
+}
